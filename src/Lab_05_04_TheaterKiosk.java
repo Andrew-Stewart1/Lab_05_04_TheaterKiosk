4e1b8c1a8506a49232f782f9a8f0c10389d79745
Andrew <stewa2a8@mail.uc.edu>
2022-09-23 11:48:52 -0400

    completed cashier question for over 21 or not

---
+++ src/Lab_05_04_TheaterKiosk.java
@@ -0,0 +1,13 @@
+public class Lab_05_04_TheaterKiosk {
+    public Lab_05_04_TheaterKiosk() {
+    }
+
+    public static void main(String[] args) {
+        int age = 22;
+        if (age >= 21) {
+            System.out.println("You get a wristband. Have a good time!");
+        } else if (age < 21) {
+            System.out.println();
+        }
+    }
+}
